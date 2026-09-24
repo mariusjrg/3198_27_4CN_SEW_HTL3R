3ca818faeec0e10d2ea98edb5cab6a43a83c2b4a
Marius Jörg <m.joerg@gmx.at>
2026-09-24 15:01:49 +0200

    Aufgabe 1 Joerg

---
+++  01_py_intro/palindrom.py
@@ -0,0 +1,9 @@
+__author__ = "Marius Jörg"
+__example__ = "SEW/01/F1" #Gegenstand/Übungsblatt/Aufgabe(Kapitel)
+__date__ = "24.09.2026"
+__version__ = "1.2.0"
+__license__ = "GNU GPLv3"
+__status__ = "Released"
+
+
+if __name__ == "__main__":
